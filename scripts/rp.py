f43784a502fcbbf1e89f79ece106edd47bb604bc
Michal Kolář <michal.kolar@designeo.cz>
2024-08-02 12:22:45 +0200

    feat:  base64 mask - convert input image to RGB

--- scripts/rp.py
+++ scripts/rp.py
@@ -412,11 +412,8 @@ class Script(modules.scripts.Script):
                     image = Image.open(BytesIO(base64.b64decode(polymask)))
                 except:
                     print("Error: The mask image is either not a valid path or not a valid base64 encoded image.")
-            try:
-                if image is not None:
-                    polymask,_,_ = draw_image(np.array(image))
-            except:
-                pass
+            if image is not None:
+                polymask,_,_ = draw_image(np.array(image.convert('RGB')))
         
         if rp_selected_tab == "Nope": rp_selected_tab = "Matrix"
 
